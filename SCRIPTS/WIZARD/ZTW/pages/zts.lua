c7f88c36b681fd6bb1d098b60a36e96118aad572
zebbel <zebbel@gmx.de>
2024-10-30 11:24:32 +0100

    changed ztw settings from table-value to table and changed naming from settingTable to setting

--- SCRIPTS/WIZARD/ZTW/pages/zts.lua
+++ SCRIPTS/WIZARD/ZTW/pages/zts.lua
@@ -15,19 +15,19 @@ ztsSettings["zts"] = {
 
 -- batIndicator sub menu
 batIndicatorMenu = {
-    {enable=1, name=language.batIndicator, type=CHECKBOX, settingTable={"zts","batIndicator"}, value="enable"},
-    {enable={"zts","batIndicator","enable"}, name=language.alarm, type=CHECKBOX, settingTable={"zts","batIndicator"}, value="alarm"},
-    {enable={"zts","batIndicator","alarm"}, name=language.minCell, type=VALUE, min=3.0, max=4.5, step=0.1, settingTable={"zts","batIndicator"}, value="minCell"}
+    {enable=1, name=language.batIndicator, type=CHECKBOX, setting={"zts","batIndicator","enable"}},
+    {enable={"zts","batIndicator","enable"}, name=language.alarm, type=CHECKBOX, setting={"zts","batIndicator","alarm"}},
+    {enable={"zts","batIndicator","alarm"}, name=language.minCell, type=VALUE, min=3.0, max=4.5, step=0.1, setting={"zts","batIndicator","minCell"}}
 }
 
 timerMenu = {
-    {enable=1, name=language.ztmTimer, type=CHECKBOX, settingTable={"zts","timer"}, value="enable"},
-    --{enable={"zts","timer","enable"}, name=language.ztsStartSwitch, type=TEST, settingTable={"zts","timer"}, value="start"}
+    {enable=1, name=language.ztmTimer, type=CHECKBOX, setting={"zts","timer","enable"}},
+    --{enable={"zts","timer","enable"}, name=language.ztsStartSwitch, type=TEST, setting={"zts","timer","start"}}
 }
 
 -- ztsPages sub menu
 ztsPages = {
-    {enable=1, name=language.ztsOutputPage, type=CHECKBOX, settingTable={"zts", "pages"}, value="output"}
+    {enable=1, name=language.ztsOutputPage, type=CHECKBOX, setting={"zts", "pages", "output"}}
 }
 
 -- zts page
